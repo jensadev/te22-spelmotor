04da620218f19487072dabd92e032e7014932edc
Jens Andreasson <jens.andreasson@ntig.se>
2024-09-23 08:15:30 +0200

    Refactor player movement logic for improved clarity and efficiency

--- src/Player.js
+++ src/Player.js
@@ -4,24 +4,16 @@ export default class Player extends GameObject {
   constructor(game) {
     super(0, 0, 50, 50, '#fff', 10)
     this.game = game
-    this.speedX = 0
-    this.maxSpeed = 5
+    this.speed = 5
   }
 
   update(deltaTime) {
-    let newX = this.x
- 
     if (this.game.keys.has("ArrowLeft")) {
-      newX = newX - this.speed
+      this.x -= this.speed
+    } else if (this.game.keys.has("ArrowRight")) {
+      this.x += this.speed
     }
-    if (this.game.keys.has("ArrowRight")) {
-      newX = newX + this.speed
-    }
-
-    this.x += newX
 
     console.log(this.x)
   }
-
-
 }
