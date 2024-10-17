67977e980235f278bdffcce4e16cb67fa23247cb
jensadev <jensandreasson77@gmail.com>
2024-10-17 22:31:02 +0200

    Refactor player movement logic for improved efficiency and clarity

--- src/Game.js
+++ src/Game.js
@@ -16,17 +16,15 @@ export default class Game {
 
 
     this.gameObjects = [
-      new GameObject(0, 100, 20, 20, '#f00', 100),
-      new GameObject(0, 200, 20, 20, '#0f0', 200),
-      new GameObject(0, 300, 20, 20, '#00f', 300)
+      new GameObject(this, 0, 100, 20, 20, '#f00', 100),
+      new GameObject(this, 0, 200, 20, 20, '#0f0', 200),
+      new GameObject(this, 0, 300, 20, 20, '#00f', 300)
     ]
   }
 
   update(deltaTime) {
     this.gameObjects.forEach(gameObject => {
-      gameObject.x = gameObject.x + gameObject.speed / 1000 * deltaTime * gameObject.direction
-      if (gameObject.x > this.width) gameObject.direction = -1
-      if (gameObject.x < 0) gameObject.direction = 1
+      gameObject.update(deltaTime)
     })
 
     this.player.update(deltaTime)
