6274ebda66591b0c129c5001bd8eda74c4772643
jensadev <jensandreasson77@gmail.com>
2024-10-17 22:30:16 +0200

    Refactor player movement logic for improved efficiency and clarity

--- src/Player.js
+++ src/Player.js
@@ -2,64 +2,37 @@ import GameObject from "./GameObject"
 
 export default class Player extends GameObject {
   constructor(game) {
-    super(0, 0, 50, 50, "#fff", 10)
-    this.game = game
+    super(game, 0, 0, 50, 50, "#fff", 10)
 
     this.image = new Image()
     this.image.src = "./src/assets/AnimationSheet_Character.png"
 
-    this.targetX = this.x;
-    this.targetY = this.y;
-
-    this.frameWidth = 32
-    this.frameHeight = 32
-    this.frameX = 0
-    this.frameY = 0
-
-    this.maxFrame = 2
-    this.fps = 10
-    this.timer = 0
-    this.interval = 1000 / this.fps
+    this.speedX = 0
+    this.speedY = 0
   }
 
   update(deltaTime) {
-    // Update target positions based on arrow keys
     if (this.game.keys.has("ArrowLeft")) {
-      this.targetX -= this.speed;
+      this.speedX = -this.maxSpeed
     } else if (this.game.keys.has("ArrowRight")) {
-      this.targetX += this.speed;
+      this.speedX = this.maxSpeed
+    } else {
+      this.speedX = 0
     }
 
     if (this.game.keys.has("ArrowUp")) {
-      this.targetY -= this.speed;
+      this.speedY = -this.maxSpeed
     } else if (this.game.keys.has("ArrowDown")) {
-      this.targetY += this.speed;
-    }
-
-
-    // Apply tweening
-    this.x += (this.targetX - this.x) * 0.1;
-    this.y += (this.targetY - this.y) * 0.1;
-
-    if (this.timer > this.interval) {
-      this.frameX++
-      this.timer = 0
+      this.speedY = this.maxSpeed
     } else {
-      this.timer += deltaTime
+      this.speedY = 0
     }
 
-    if (this.frameX >= this.maxFrame) {
-      this.frameX = 0
-    }
-
-    if (Math.abs(this.targetX - this.x) > 0.5 || Math.abs(this.targetY - this.y) > 0.5) {
-      this.frameY = 3;
-    } else {
-      this.frameY = 0;
-    }
+    this.x += this.speedX
+    this.y += this.speedY
   }
 
   draw(ctx) {
-    ctx.drawImage(this.image, this.frameX * this.frameWidth, this.frameY * this.frameHeight, this.frameWidth, this.frameHeight, this.x, this.y, this.width, this.height)
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
   }
 }
