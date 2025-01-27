828d2e9fcab64e7478246cf553701d32887a1d77
Jens Andreasson <jens.andreasson@ntig.se>
2025-01-27 11:11:11 +0100

    Implement scoring system and display game over message; adjust enemy spawn interval based on game time

--- src/Game.js
+++ src/Game.js
@@ -22,13 +22,13 @@ export default class Game {
     this.groundHeight = 80
     this.gravity = 0.5
 
-    this.gameObjects = [
-    ]
+    this.gameObjects = []
 
     this.gameOver = false
     this.gameTimer = 0
     this.enemyTimer = 0
     this.enemyInterval = Math.random() * 3000
+    this.score = 0
   }
 
   update(deltaTime) {
@@ -37,35 +37,71 @@ export default class Game {
     }
     this.gameTimer += deltaTime
     this.background.update(deltaTime)
-    this.gameObjects.forEach(gameObject => {
+    this.gameObjects.forEach((gameObject) => {
       gameObject.update(deltaTime)
     })
 
     this.player.update(deltaTime)
 
     if (this.enemyTimer > this.enemyInterval) {
-      this.gameObjects.push(new Obstacle(this, this.width, this.height - this.groundHeight - 60, 60, 40))
+      const minInterval = 1000
+      const maxInterval = 3000
+      const intervalDecrease = this.gameTimer / 1000
+      const newInterval = Math.max(minInterval, maxInterval - intervalDecrease)
+
+      this.gameObjects.push(
+        new Obstacle(
+          this,
+          this.width,
+          this.height - this.groundHeight - 60,
+          60,
+          40,
+        ),
+      )
       this.enemyTimer = 0
-      this.enemyInterval = Math.random() * (5000 - this.gameTimer / 100)
+      this.enemyInterval = newInterval
     } else {
       this.enemyTimer += deltaTime
     }
 
-    this.gameObjects = this.gameObjects.filter(gameObject => !gameObject.markForDeletion)
+    this.gameObjects = this.gameObjects.filter((gameObject) => {
+      if (gameObject.markForDeletion) {
+        this.score += 1
+        return false
+      }
+      return true
+    })
   }
 
   draw(ctx) {
     this.background.draw(ctx)
-    this.gameObjects.forEach(gameObject => {
+    this.gameObjects.forEach((gameObject) => {
       gameObject.draw(ctx)
-      this.gameOver = this.checkCollision(this.player, gameObject) 
+      this.gameOver = this.checkCollision(this.player, gameObject)
     })
 
     this.player.draw(ctx)
     this.background.backgroundLayers[3].draw(ctx)
+
+    if (this.gameOver) {
+      ctx.fillStyle = "black"
+      ctx.globalAlpha = 0.5
+      ctx.fillRect(0, 0, this.width, this.height)
+      ctx.globalAlpha = 1
+      ctx.fillStyle = "white"
+      ctx.font = "30px Arial"
+      ctx.fillText("Game Over", this.width / 2 - 80, this.height / 2)
+    }
+
+    ctx.fillStyle = "black"
+    ctx.font = "20px Arial"
+    ctx.fillText("Score: " + this.score, 20, 30)
+
+    const elapsedTime = (this.gameTimer / 1000).toFixed(2)
+    ctx.fillText("Time: " + elapsedTime + "s", 20, 60)
   }
 
-    checkCollision(player, obstacle) {
+  checkCollision(player, obstacle) {
     let topOfObstacle = obstacle.y
     let bottomOfObstacle = obstacle.y + obstacle.height
     let leftSideOfObstacle = obstacle.x
